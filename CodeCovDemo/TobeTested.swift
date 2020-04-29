5fda72e74be2f85a01f70e46bf1e601da4ff43a0
Natsios Konstantinos <kwstasna@hotmail.com>
2020-04-29 15:11:55 +0300

    first commit

---
+++ CodeCovDemo/TobeTested.swift
@@ -0,0 +1,27 @@
+//
+//  TobeTested.swift
+//  CodeCovDemo
+//
+//  Created by Konstantinos Natsios on 29/4/20.
+//  Copyright © 2020 Konstantinos Natsios. All rights reserved.
+//
+
+import Foundation
+
+class TestClass {
+    let a: Int
+    let b: Int
+    
+    init(a: Int, b: Int) {
+        self.a = a
+        self.b = b
+    }
+    
+    func test1() -> Int {
+        return a + b
+    }
+    
+    func test2() -> Int {
+        return a - b
+    }
+}
