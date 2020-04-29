37a18e41075be0acdd3b24525a9de32e106033b3
Natsios Konstantinos <kwstasna@hotmail.com>
2020-04-29 15:02:46 +0300

    Initial Commit

---
+++ CodeCovDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeCovDemo
+//
+//  Created by Konstantinos Natsios on 29/4/20.
+//  Copyright © 2020 Konstantinos Natsios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
